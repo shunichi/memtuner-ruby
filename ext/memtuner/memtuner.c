b2407bd6624e4d8b59ebb4cc68da44152c843df5
Shunichi Ikegami <sike.tm@gmail.com>
2017-03-24 18:55:44 +0900

    Memtuner#mallinfo

--- ext/memtuner/memtuner.c
+++ ext/memtuner/memtuner.c
@@ -11,6 +11,39 @@ VALUE rb_mMemtuner;
 static VALUE sym_current;
 static VALUE sym_peak;
 
+/* struct mallinfo members */
+static VALUE sym_arena;     /* Non-mmapped space allocated (bytes) */
+static VALUE sym_ordblks;   /* Number of free chunks */
+static VALUE sym_smblks;    /* Number of free fastbin blocks */
+static VALUE sym_hblks;     /* Number of mmapped regions */
+static VALUE sym_hblkhd;    /* Space allocated in mmapped regions (bytes) */
+static VALUE sym_usmblks;   /* Maximum total allocated space (bytes) */
+static VALUE sym_fsmblks;   /* Space in freed fastbin blocks (bytes) */
+static VALUE sym_uordblks;  /* Total allocated space (bytes) */
+static VALUE sym_fordblks;  /* Total free space (bytes) */
+static VALUE sym_keepcost;  /* Top-most, releasable space (bytes) */
+
+VALUE rb_memtuner_mallinfo(VALUE self)
+{
+#if HAVE_MALLOC_INFO
+    struct mallinfo mi = mallinfo();
+    VALUE hash = rb_hash_new();
+    rb_hash_aset(hash, sym_arena, INT2NUM(mi.arena));
+    rb_hash_aset(hash, sym_ordblks, INT2NUM(mi.ordblks));
+    rb_hash_aset(hash, sym_smblks, INT2NUM(mi.smblks));
+    rb_hash_aset(hash, sym_hblks, INT2NUM(mi.hblks));
+    rb_hash_aset(hash, sym_hblkhd, INT2NUM(mi.hblkhd));
+    rb_hash_aset(hash, sym_usmblks, INT2NUM(mi.usmblks));
+    rb_hash_aset(hash, sym_fsmblks, INT2NUM(mi.fsmblks));
+    rb_hash_aset(hash, sym_uordblks, INT2NUM(mi.uordblks));
+    rb_hash_aset(hash, sym_fordblks, INT2NUM(mi.fordblks));
+    rb_hash_aset(hash, sym_keepcost, INT2NUM(mi.keepcost));
+    return hash;
+#else
+    return Qnil;
+#endif
+}
+
 VALUE
 rb_memtuner_malloc_info(VALUE self)
 {
@@ -43,10 +76,23 @@ rb_memtuner_rss_usage(VALUE self)
 void
 Init_memtuner(void)
 {
-    sym_current = ID2SYM(rb_intern("current"));
-    sym_peak = ID2SYM(rb_intern("peak"));
+#define DEF_SYM(name) sym_ ## name = ID2SYM(rb_intern(#name))
+    DEF_SYM(current);
+    DEF_SYM(peak);
+    DEF_SYM(arena);
+    DEF_SYM(ordblks);
+    DEF_SYM(smblks);
+    DEF_SYM(hblks);
+    DEF_SYM(hblkhd);
+    DEF_SYM(usmblks);
+    DEF_SYM(fsmblks);
+    DEF_SYM(uordblks);
+    DEF_SYM(fordblks);
+    DEF_SYM(keepcost);
+#undef DEF_SYM
 
     rb_mMemtuner = rb_define_module("Memtuner");
+    rb_define_module_function(rb_mMemtuner, "mallinfo", rb_memtuner_mallinfo, 0);
     rb_define_module_function(rb_mMemtuner, "malloc_info", rb_memtuner_malloc_info, 0);
     rb_define_module_function(rb_mMemtuner, "rss_usage", rb_memtuner_rss_usage, 0);
 }
--- spec/memtuner_spec.rb
+++ spec/memtuner_spec.rb
@@ -5,6 +5,17 @@ describe Memtuner do
     expect(Memtuner::VERSION).not_to be nil
   end
 
+  describe '#mallinfo' do
+    it 'returns non nil value' do
+      expect(Memtuner.mallinfo).not_to be_nil
+    end
+    it 'has mallinfo member keys' do
+      %i(arena ordblks smblks hblks hblkhd usmblks fsmblks uordblks fordblks keepcost).each do |sym|
+        expect(Memtuner.mallinfo).to include sym
+      end
+    end
+  end
+
   describe '#malloc_info' do
     it 'returns non nil value' do
       expect(Memtuner.malloc_info).not_to be_nil
