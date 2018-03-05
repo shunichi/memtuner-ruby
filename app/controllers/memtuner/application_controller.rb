3c8554849922be21393f5e47181ccdd921ab0b95
Shunichi Ikegami <sike.tm@gmail.com>
2018-03-05 11:44:11 +0900

    Add basic auth

--- app/controllers/memtuner/application_controller.rb
+++ app/controllers/memtuner/application_controller.rb
@@ -1,5 +1,9 @@
 module Memtuner
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+
+    if ENV["MEMTUNER_PASSWORD"]
+      http_basic_authenticate_with name: ENV["MEMTUNER_USERNAME"], password: ENV["MEMTUNER_PASSWORD"]
+    end
   end
 end
